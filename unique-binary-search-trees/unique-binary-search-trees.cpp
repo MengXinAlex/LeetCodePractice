76bfa20895e5c38f5ac97d50df8fbdbacf87b59c
Xianfeng Zhuge <46865258+MengXinAlex@users.noreply.github.com>
2021-11-08 21:39:13 +1100

    Time: 0 ms (100.00%), Space: 6 MB (37.05%) - LeetHub

---
+++ unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1);
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            int sum=0;
+            for(int j=0;j<i;j++)
+                sum+=(dp[j]*dp[i-1-j]);
+            dp[i]=sum;
+        }
+        return dp[n];
+    }
+};
