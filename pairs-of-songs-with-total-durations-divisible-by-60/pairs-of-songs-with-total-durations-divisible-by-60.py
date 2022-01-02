9c6259d1b4011aadc9ee6b4e308df4a8e2bf26a0
Xianfeng Zhuge <46865258+MengXinAlex@users.noreply.github.com>
2022-01-02 23:24:47 +1100

    Time: 213 ms (32.91%), Space: 16.6 MB (18.23%) - LeetHub

---
+++ pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def numPairsDivisibleBy60(self, time):
+        hashmap = defaultdict(int)
+        for t in time:
+            hashmap[t%60] += 1
+        
+        res = 0
+        for t in time:
+            reminder = t%60
+            hashmap[reminder] -= 1
+                
+            res += hashmap[0 if reminder == 0 else 60 - reminder]
+                
+        return res
